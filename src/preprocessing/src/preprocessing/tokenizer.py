12b42035299faee0c44d65be17e42714adb39513
KRISHNA SAI ALLU <150152859+krsna1206@users.noreply.github.com>
2025-12-25 22:52:09 +0530

    unpacked src folder folder all logic available in preprocessing folder

--- src/preprocessing/src/preprocessing/tokenizer.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-
-
-def tokenize(text: str) -> List[str]:
-    """Simple whitespace tokenizer. Returns empty list on empty input."""
-    if not text:
-        return []
-    return text.split()
